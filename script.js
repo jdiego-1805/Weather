da86888245448ba91bfd4fa372a8107dfd21b39c
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-04-16 11:27:42 -0500

    added css and some header

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 
 
 
-fetch ("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid="){
-    .then(Response => response.json())
+fetch ("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5"){
+    .then(response => response.json())
     .then(citiesFound => {
-        let firstCity = citiesFound[0]
+        let firstCity = citiesFound[0];
         console.log(firstCity.lat);
         console.log(firstCity.lon);
     
-        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=')
+        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=')
     })
 
 })
