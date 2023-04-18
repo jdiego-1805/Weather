8455cbf935dc4ec1555f203bd1b2a280962c7584
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-04-17 20:31:39 -0500

    made fetch happen ;)

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
+var APIkey = "5d7e85acfb64463dbf6905d193511b21"
+var searchBtn = document.getElementById("btn");
+var cityInput = document.getElementById("city-input");
 
 
-
-
-
-
-
-fetch ("https://api.openweathermap.org/geo/1.0/direct?q=${Dallas}&limit=55d7e85acfb64463dbf6905d193511b21"){
+function searchCurrentWeather() {
+    var city = cityInput.value
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIkey+"&units=imperial")
     .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
-    
-        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=5d7e85acfb64463dbf6905d193511b21')
+    .then((data) => {
+        console.log(data);
+        var icon = "https://openweathermap.org/img/wn/"+data.weather.icon+"@2x.png"
     })
+}
 
-})
 
-.then(response => response.json())
-.then(data => {
 
-    console.log(data);
-})
+
+// second API call https://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=5d7e85acfb64463dbf6905d193511b21
+
+searchBtn.addEventListener ("click", searchCurrentWeather)
+
+
+
+
+
+
+// fetch("https://api.openweathermap.org/geo/1.0/direct?q=${Dallas}&limit=55d7e85acfb64463dbf6905d193511b21"){
+//     .then(response => response.json())
+//         .then(citiesFound => {
+//             let firstCity = citiesFound[0];
+//             console.log(firstCity.lat);
+//             console.log(firstCity.lon);
+
+//             return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=5d7e85acfb64463dbf6905d193511b21')
+//         })
+
+// }
+
+// .then(response => response.json())
+//     .then(data => {
+
+//         console.log(data);
+//     })
+
